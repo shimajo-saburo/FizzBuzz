68938fd1772fe102e1b439df1236f241f19d949a
清水　美佑 <89442144+shimajo-saburo@users.noreply.github.com>
2021-08-25 10:44:25 +0900

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,13 @@
-test
+for num in 1..30 do
+    if num%3==0 then
+        if num%5==0 then
+            print("FizzBuzz", "\n")
+        else
+            print("Fizz", "\n")
+        end
+    elsif num%5==0 then 
+        print("Buzz", "\n")
+    else 
+        print(num,"\n")
+    end
+end
